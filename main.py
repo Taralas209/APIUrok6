f144e6bf980eccc6d3f2bf81fb573a534d207cd2
Taralas209 <alexej.zikunow@gmail.com>
2023-08-03 11:58:06 +0200

    rename some varaibles

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import random
 import requests
-import pprint
 from dotenv import load_dotenv
 
 
@@ -19,9 +18,9 @@ def download_random_comic(last_comic_number):
     comic_response = requests.get(comic_url)
     comic_response.raise_for_status()
 
-    comic_data = comic_response.json()
-    comic_image_url = comic_data['img']
-    comic_comments = comic_data['alt']
+    comic_details = comic_response.json()
+    comic_image_url = comic_details['img']
+    comic_comments = comic_details['alt']
     comic_image_name = f"""comic_{random_comic_number}.jpg"""
 
     image_response = requests.get(comic_image_url)
@@ -68,7 +67,7 @@ def upload_photo_to_vk_server(vk_server_upload_url, comic_image_name):
 def save_photo_to_vk_wall(vk_group_id, vk_access_token, uploaded_photo_parameters, uploaded_server, uploaded_hash):
     method = 'photos.saveWallPhoto'
     vk_url = f'''https://api.vk.com/method/{method}'''
-    data = {
+    vk_wall_settings = {
         "group_id": vk_group_id,
         "access_token": vk_access_token,
         "v": "5.131",
@@ -76,7 +75,7 @@ def save_photo_to_vk_wall(vk_group_id, vk_access_token, uploaded_photo_parameter
         "server": uploaded_server,
         "hash": uploaded_hash,
     }
-    vk_response = requests.post(vk_url, data=data)
+    vk_response = requests.post(vk_url, data=vk_wall_settings)
     check_vk_response(vk_response)
     saved_photo_details = vk_response.json()['response'][0]
     saved_photo_owner_id = saved_photo_details['owner_id']
@@ -85,7 +84,7 @@ def save_photo_to_vk_wall(vk_group_id, vk_access_token, uploaded_photo_parameter
 
 
 
-def public_post_to_vk_group_wall(vk_group_id, vk_access_token, saved_photo_owner_id, saved_photo_id, comments):
+def publish_post_to_vk_group_wall(vk_group_id, vk_access_token, saved_photo_owner_id, saved_photo_id, comments):
     method = 'wall.post'
     vk_url = f'''https://api.vk.com/method/{method}'''
     attachments = f"photo{saved_photo_owner_id}_{saved_photo_id}"
@@ -117,7 +116,7 @@ def main():
             os.remove(comic_image_name)
 
     saved_photo_owner_id, saved_photo_id = save_photo_to_vk_wall(vk_group_id, vk_access_token, uploaded_photo_parameters, uploaded_server, uploaded_hash)
-    public_post_to_vk_group_wall(vk_group_id, vk_access_token, saved_photo_owner_id, saved_photo_id, comic_comments)
+    publish_post_to_vk_group_wall(vk_group_id, vk_access_token, saved_photo_owner_id, saved_photo_id, comic_comments)
 
 
 if __name__ == "__main__":
